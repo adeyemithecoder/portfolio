4d20b9ca0aafa522616bae74347d4262096e9d0c
Mathew Adeyemi <adeyemithecoder@gmail.com>
2026-08-28 12:36:31 +0100

    last commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -80,7 +80,6 @@ const DEPLOY_FLOW = [
 const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
 const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
 const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
-
 const initialForm = { name: "", email: "", subject: "", message: "" };
 
 function validate(values) {
